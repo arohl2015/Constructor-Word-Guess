5457ffce86722f32658034114183793b1dd4ff34
A Rohl <april.rohl2015@gmail.com>
2020-04-11 19:30:30 -0700

    letter.js logic completed

--- letter.js
+++ letter.js
@@ -4,14 +4,36 @@
 
 // A string value to store the underlying character for the letter
 function Letter(character) {
-    this.Letter = character
-// A boolean value that stores whether that letter has been guessed yet
-    this.guessed = false
-// A function that returns the underlying character if the letter has 
-// been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+    this.Letter = character;
+    // A boolean value that stores whether that letter has been guessed yet
+    this.guessed = false;
+    // A function that returns the underlying character if the letter has 
+    // been guessed, or a placeholder (like an underscore) if the letter has not been guessed
     this.toString = function () {
-
-    }
-// A function that takes a character as an argument and checks it against the underlying character, 
-// updating the stored boolean value to true if it was guessed correctly
+        if (this.letter === " ") {
+            this.guessed = true;
+            return " ";
+        }
+        else {
+            if (this.guessed === false) {
+                return "_";
+            }
+            else {
+                return this.letter;
+            }
+        }
+    };
+    // A function that takes a character as an argument and checks it against the underlying character, 
+    // updating the stored boolean value to true if it was guessed correctly
+    this.guess = function (guess) {
+        if (guess === this.letter) {
+            this.guessed = true;
+        }
+    };
 }
+
+// Exporting our letter constructor. We will require it in word.js
+module.exports = Letter;
+
+
+
